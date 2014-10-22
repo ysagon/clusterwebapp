7bd9e4e585af36130fb99bbdf91819a1251627d6
Yann Sagon <Yann.Sagon@unige.ch>
2014-10-22 11:04:26 +0200

    bugfix parsing date with more than 9 days

--- slurm.js
+++ slurm.js
@@ -6,12 +6,12 @@
 
 /**
  * Use sacct to extract informations about the terminated jobs of a user
- * @param {string} value time in slurm format (J-HH:MM:SS)
+ * @param {string} value time in slurm format (JJ-HH:MM:SS)
  * @return {mixed} nb of sec or false
  */
 exports.convertSlurmDateToSec = function(value) {
-    // pattern to match a slurm date: d-h:m:s
-    var pattern = /^(?:([0-9])-)?(?:(\d\d):)?(?:(\d\d):)?(\d\d)/;
+    // pattern to match a slurm date: JJ-HH:MM:SS
+    var pattern = /^(?:([0-9]{1,2})-)?(?:(\d\d):)?(?:(\d\d):)?(\d\d)/;
     // 1 day = 24 * 60 * 60
     // 1 hour = 60 * 60
     // 1 min = 60
